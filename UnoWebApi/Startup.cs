b91a44de12436fa66ad3252e0729fc319301dfc8
gideon.c <gideon.c@ITPL-SOFT-001>
2020-02-04 16:03:02 +0530

    Add project files.

---
+++ UnoWebApi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UnoWebApi.Startup))]
+namespace UnoWebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
